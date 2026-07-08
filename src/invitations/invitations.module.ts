0e602ba1de63aeae3c1bc353ec74a3a24a41053c
enoch B <henokt828@gmail.com>
2026-07-08 09:44:49 +0300

    feat(invitations): add DepartmentsModule to invitations module imports

--- src/invitations/invitations.module.ts
+++ src/invitations/invitations.module.ts
@@ -4,11 +4,13 @@ import { InvitationsService } from './invitations.service.js';
 import { InvitationsController } from './invitations.controller.js';
 import { UserInvitation } from './entities/user-invitation.entity.js';
 import { UsersModule } from '../users/users.module.js';
+import { DepartmentsModule } from '../department/department.module.js';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserInvitation]),
     UsersModule,
+    DepartmentsModule
   ],
   controllers: [InvitationsController],
   providers: [InvitationsService],
