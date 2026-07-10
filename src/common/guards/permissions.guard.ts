d8f9a7b35bf03e1429a3fd00131fbf30be6838a4
enoch B <henokt828@gmail.com>
2026-07-10 09:44:49 +0300

    fix: remove unnecessary file extension from permissions.decorator import

--- src/common/guards/permissions.guard.ts
+++ src/common/guards/permissions.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { PERMISSIONS_KEY } from '../decorators/permissions.decorator.js';
+import { PERMISSIONS_KEY } from '../decorators/permissions.decorator';
 
 @Injectable()
 export class PermissionsGuard implements CanActivate {
