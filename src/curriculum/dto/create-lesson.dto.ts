7f1c28cf4c6e6148d84ffb7248d291b272df50bb
enoch B <henokt828@gmail.com>
2026-07-10 21:46:39 +0300

    feat: enhance DTOs with ApiProperty decorators for improved Swagger documentation

--- src/curriculum/dto/create-lesson.dto.ts
+++ src/curriculum/dto/create-lesson.dto.ts
@@ -1,17 +1,22 @@
 import { IsString, IsNotEmpty, IsOptional, IsInt, IsBoolean } from 'class-validator';
+import { ApiProperty } from 'node_modules/@nestjs/swagger/dist/decorators/api-property.decorator';
 
 export class CreateLessonDto {
+  @ApiProperty({ example:'Lesson 1: Introduction to Programming', description: 'The title of the lesson' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ example:'<p>This is the content of Lesson 1.</p>', description: 'The HTML content of the lesson' })
   @IsString()
   @IsNotEmpty()
   contentHtml: string;
 
+  @ApiProperty({ example: 1, description: 'The order of the lesson in the chapter' })
   @IsInt()
   @IsOptional()
   sequenceOrder?: number;
+
 
   @IsBoolean()
   @IsOptional()
--- src/curriculum/dto/create-question.dto.ts
+++ src/curriculum/dto/create-question.dto.ts
@@ -1,25 +1,31 @@
 import { IsString, IsNotEmpty, IsInt, IsOptional, IsArray, ValidateNested, ArrayMinSize, IsBoolean } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAnswerDto {
+  @ApiProperty({ example: 'Answer 1', description: 'The text of the answer' })
   @IsString()
   @IsNotEmpty()
   text: string;
 
+  @ApiProperty({ example: false, description: 'Indicates if the answer is correct' })
   @IsBoolean()
   @IsNotEmpty()
   isCorrect: boolean;
 }
 
 export class CreateQuestionDto {
+  @ApiProperty({ example: 'What is the capital of France?', description: 'The text of the question' })
   @IsString()
   @IsNotEmpty()
   questionText: string;
 
+  @ApiProperty({ example: 10, description: 'The number of points the question is worth' })
   @IsInt()
   @IsNotEmpty()
   points: number;
 
+  @ApiProperty({ example: 1, description: 'The order of the question in the quiz' })
   @IsInt()
   @IsOptional()
   sequenceOrder?: number;
