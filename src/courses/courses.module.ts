b20ab60c85b40b1e7fa80b40a409de627212ab57
enoch B <henokt828@gmail.com>
2026-07-10 09:45:09 +0300

    feat: add CommonModule to CoursesModule imports for improved functionality

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,11 +4,13 @@ import { Course } from './entities/course.entity';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { DepartmentsModule } from '../departments/department.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Course]),
     DepartmentsModule,
+    CommonModule,
   ],
   controllers: [CoursesController],
   providers: [CoursesService],
