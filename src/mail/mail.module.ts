9177d1064a45e2784cd189ae8878fca0fdfdfc5d
enoch <henokt828@gmail.com>
2026-07-14 11:57:03 +0300

    feat: update MailModule to include MailProcessor and improve email sender configuration

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,32 +1,49 @@
-import { Global, Module } from '@nestjs/common';
+// src/mail/mail.module.ts
+import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
-import { MailService } from './mail.service.js';
+import { MailService } from './mail.service';
+import { MailProcessor } from '../queue/mail.process';
 
-@Global()
 @Module({
   imports: [
+    // BullMQ Queue registration
+    BullModule.registerQueueAsync({
+      name: 'email',
+      useFactory: async (configService: ConfigService) => ({
+        connection: {
+         url: configService.get<string>('REDIS_URL') || 'redis://localhost:6379',
+        },
+        defaultJobOptions: {
+          attempts: 3,
+          backoff: {
+            type: 'exponential',
+            delay: 2000,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
     MailerModule.forRootAsync({
       useFactory: async (config: ConfigService) => ({
         transport: {
           host: config.get('SMTP_HOST'),
           port: config.get('SMTP_PORT'),
           secure: config.get('SMTP_SECURE') === 'true',
-          family: 4, 
-
           auth: {
             user: config.get('SMTP_USER'),
             pass: config.get('SMTP_PASS'),
           },
         },
         defaults: {
-          from: `"No Reply" <${config.get('SMTP_USER')}>`,
+          from: `"Edu3" <${config.get('SMTP_USER')}>`,
         },
       }),
       inject: [ConfigService],
     }),
   ],
-  providers: [MailService],
+  providers: [MailService, MailProcessor],
   exports: [MailService],
 })
 export class MailModule {}
